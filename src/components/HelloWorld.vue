5d4857d3e5919d962546c06a4380b891d6f6b5d7
Ctushar721 <tushar.choudhary010@gmail.com>
2025-06-08 16:34:37 +0530

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ props.msg }}</h1>
+
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
